02f5feba8a46631030bbd4a32a31122c703a293f
tejas <tejas@Edward>
2022-11-22 23:41:08 +0530

    asd

---
+++ src/jUnitTestPackage/jUnitTestFunctions.java
@@ -0,0 +1,16 @@
+package jUnitTestPackage;
+
+import java.util.Scanner;
+
+public class jUnitTestFunctions{
+	public static void addnum(int x,int y){
+		int sum = x + y;  
+		System.out.println("Sum is: " + sum);}
+	
+	public static void addstring(String a, String b){
+		String ne=a+b;
+		System.out.println("New String is: " + ne);
+		}	
+
+}
+//asd
---
+++ src/jUnitTestPackage/testAddNumbers.java
@@ -0,0 +1,24 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		    int x, y;
+		    Scanner s = new Scanner(System.in); 
+		    System.out.println("Type a number:");
+		    x = s.nextInt(); 
+		    System.out.println("Type another number:");
+		    y = s.nextInt(); 
+		    jUnitTestFunctions ob = new jUnitTestFunctions();
+		    ob.addnum(x, y);
+	}
+
+}
+//asd
